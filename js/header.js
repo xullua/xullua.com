ae0d69aa2eb9a5565e88fc81ee446c9c0a10f606
ぞるあ <110039162+xullua@users.noreply.github.com>
2022-09-25 18:43:32 +0900

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,12 +1,12 @@
 $(function () {
-    var hd_size = $('header').innerHeight(); //ヘッダーの高さを取得
+    var hd_size = $('header').innerHeight();
     var pos = 0;
     $(window).on('scroll', function () {
-        var current_pos = $(this).scrollTop(); //現在の位置を取得
-        if (current_pos < pos || current_pos == 0) { //上にスクロール もしくは 最上部
-            $('header').css({ 'top': 0 }); //ヘッダーを表示
+        var current_pos = $(this).scrollTop();
+        if (current_pos < pos || current_pos == 0) { 
+            $('header').css({ 'top': 0 });
         } else {
-            $('header').css({ 'top': '-' + hd_size + 'px' }); //ヘッダーを非表示(下にスクロール)
+            $('header').css({ 'top': '-' + hd_size + 'px' });
         }
         pos = current_pos;
     });
