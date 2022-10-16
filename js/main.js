1e09fca593ba69b089a64a44f528c83607032037
ぞるあ <110039162+xullua@users.noreply.github.com>
2022-10-16 14:19:18 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ $(function () {
         pos = current_pos;
     });
 });
+
+$(function () {
+    var hd_size = $('header').innerHeight();
+    var pos = 0;
+    $(window).on('scroll', function () {
+        var current_pos = $(this).scrollTop();
+        if (current_pos < pos || current_pos == 0) {
+            document.getElementById('mokuji').classList.add('active')
+        } else {
+            document.getElementById('mokuji').classList.remove('active')
+        }
+        pos = current_pos;
+    });
+});
