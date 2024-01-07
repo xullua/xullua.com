7d2ac7c471d0622bea0af4a1fc6dbb29d8b961c0
ぞるあ <110039162+xullua@users.noreply.github.com>
2024-01-07 23:50:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -25,3 +25,27 @@ window.onload = function () {
         }
     });
 }
+
+
+//Androidの場合別のフォントを読み込む
+// ユーザーエージェントを取得する関数
+function getOS() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+    return "Other";
+}
+
+// OSに応じてフォントを設定する関数
+function setFont() {
+    const os = getOS();
+    if (os === "Android") {
+        // Androidの場合はGoogle FontsのNoto Sans Japaneseを読み込む
+        const link = document.createElement('link');
+        link.href = 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;500;700;900&family=Zen+Maru+Gothic:wght@400;500;700;900&display=swap';
+        link.rel = 'stylesheet';
+        document.head.appendChild(link);
+    }
+}
+window.onload = setFont;
