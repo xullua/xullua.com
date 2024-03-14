ea8cfad4dda4948db549d1573c45f4f452baffc0
ぞるあ <110039162+xullua@users.noreply.github.com>
2024-03-14 15:21:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-//ヘッダースクロールの表示非表示
-// ページが読み込まれた時に実行される処理
-document.getElementById('header').classList.add('active')
-window.onload = function () {
-    var prevScrollpos = window.pageYOffset;
-    var ticking = false;
-    // スクロールイベントを監視し、headerの表示・非表示を切り替える
-    window.addEventListener('scroll', function () {
-        var currentScrollPos = window.pageYOffset;
-        if (!ticking) {
-            window.requestAnimationFrame(function () {
-                // スクロール速度を計算
-                var scrollSpeed = Math.abs(currentScrollPos - prevScrollpos);
-                if (scrollSpeed > 30) {
-                    if (prevScrollpos > currentScrollPos) {
-                        document.getElementById('header').classList.add('active')
-                    } else {
-                        document.getElementById('header').classList.remove('active')
-                    }
-                }
-                prevScrollpos = currentScrollPos;
-                ticking = false;
-            });
-            ticking = true;
-        }
-    });
-}
-
-
 //Androidの場合別のフォントを読み込む
 // ユーザーエージェントを取得する関数
 function getOS() {
