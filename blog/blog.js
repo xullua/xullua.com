66d8a2388199ea0c395228cc75ef239d49bdf52e
ぞるあ <110039162+xullua@users.noreply.github.com>
2024-03-19 18:44:57 +0900

    Delete blog directory

--- blog/blog.js
+++
@@ -1,1 +0,0 @@
-fetch("blog.json") .then(function (response){return response.json();}) .then(function (blogs){let placeholder=document.querySelector("#output"); let out=""; for (let blog of blogs){out +=`<div class="blogbox"> <a href="${blog.url}"> <img src="${blog.img}"> <h3>${blog.title}</h3> </a> </div>`;}placeholder.innerHTML=out;});
