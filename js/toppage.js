da28303d40d4928dcf3ce00b3a767b0a983eb94a
ぞるあ <110039162+xullua@users.noreply.github.com>
2024-03-19 18:44:07 +0900

    Add files via upload

--- js/toppage.js
+++ js/toppage.js
@@ -2,12 +2,12 @@
 if ($('.top-hello').length) {
     var imgpass = "画像ファイルまでのパス";
     var imgfile = [];
-    imgfile[0] = 'img/top-hello1.webp';
-    imgfile[1] = 'img/top-hello2.webp';
-    imgfile[2] = 'img/top-hello3.webp';
-    imgfile[3] = 'img/top-hello4.webp';
-    imgfile[4] = 'img/top-hello5.webp';
-    imgfile[5] = 'img/top-hello6.webp';
+    imgfile[0] = '//xullua.com/img/top-hello1.webp';
+    imgfile[1] = '//xullua.com/img/top-hello2.webp';
+    imgfile[2] = '//xullua.com/img/top-hello3.webp';
+    imgfile[3] = '//xullua.com/img/top-hello4.webp';
+    imgfile[4] = '//xullua.com/img/top-hello5.webp';
+    imgfile[5] = '//xullua.com/img/top-hello6.webp';
     var n = Math.floor(Math.random() * imgfile.length);
     var bgbox = $('.top-hello');
     bgbox.css('background-image', 'url(' + imgfile[n] + ')');
@@ -34,7 +34,7 @@ function changeTime() {
 }
 changeTime();
 
-fetch('js/top-words.json')
+fetch('//xullua.com/js/top-words.json')
   .then(response => response.json())
   .then(data => {
     // `data` に JSON データが格納されます
