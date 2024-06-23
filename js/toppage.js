af496e2be6ec2e18db402f905d1fece621309714
ぞるあ <110039162+xullua@users.noreply.github.com>
2024-06-23 11:33:17 +0900

    Add files via upload

--- js/toppage.js
+++ js/toppage.js
@@ -13,39 +13,58 @@ if ($('.top-hello').length) {
     bgbox.css('background-image', 'url(' + imgfile[n] + ')');
 }
 
-//時間ごとのあいさつ
+
+// 時間ごとのあいさつ
 var d = new Date();
 var hours = d.getHours();
-var greeting = document.getElementById("greeting")
+var greeting = document.getElementById("greeting");
 var b = document.getElementsByTagName('body')[0];
-function changeTime() {
-    if (6 < hours && hours <= 10) {
-        greeting.textContent = "おはよう";
-    }
-    else if (10 < hours && hours <= 17) {
-        greeting.textContent = "こんにちは";
-    }
-    else if (17 < hours && hours <= 23) {
-        greeting.textContent = "こんばんは";
-    }
-    else if (hours < 5) {
-        greeting.textContent = "早く寝ろ";
+
+function getGreetingMessage(hours, isEnglish) {
+    if (isEnglish) {
+        if (6 < hours && hours <= 10) {
+            return "Morning";
+        } else if (10 < hours && hours <= 17) {
+            return "Good Afternoon";
+        } else if (17 < hours && hours <= 23) {
+            return "Good Evening";
+        } else if (hours < 5) {
+            return "Go to bed";
+        }
+    } else {
+        if (6 < hours && hours <= 10) {
+            return "おはよう";
+        } else if (10 < hours && hours <= 17) {
+            return "こんにちは";
+        } else if (17 < hours && hours <= 23) {
+            return "こんばんは";
+        } else if (hours < 5) {
+            return "早く寝ろ";
+        }
     }
 }
+
+function changeTime() {
+    var isEnglish = window.location.pathname.includes('/en/');
+    greeting.textContent = getGreetingMessage(hours, isEnglish);
+}
+
 changeTime();
 
-fetch('//xullua.com/js/top-words.json')
-  .then(response => response.json())
-  .then(data => {
-    // `data` に JSON データが格納されます
-    const randomIndex = Math.floor(Math.random() * data.length); // ランダムなインデックスを生成
-    const randomWord = data[randomIndex]; // ランダムに選んだ単語オブジェクトを取得
 
-    // `text` と `name` を HTML ページに表示する
-    const textElement = document.querySelector('#top-words-text');
-    textElement.textContent = randomWord.text;
 
-    const nameElement = document.querySelector('#top-words-name');
-    nameElement.textContent = randomWord.name;
-  })
-  .catch(error => console.error(error));
+fetch('https://xullua.com/js/top-words.json')
+    .then(response => response.json())
+    .then(data => {
+        // `data` に JSON データが格納されます
+        const randomIndex = Math.floor(Math.random() * data.length); // ランダムなインデックスを生成
+        const randomWord = data[randomIndex]; // ランダムに選んだ単語オブジェクトを取得
+
+        // `text` と `name` を HTML ページに表示する
+        const textElement = document.querySelector('#top-words-text');
+        textElement.textContent = randomWord.text;
+
+        const nameElement = document.querySelector('#top-words-name');
+        nameElement.textContent = randomWord.name;
+    })
+    .catch(error => console.error(error));
