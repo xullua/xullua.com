d4ab70c8e7c12b737506e69db8bbcba37302c069
ぞるあ <110039162+xullua@users.noreply.github.com>
2024-08-15 14:56:44 +0900

    Add files via upload

--- js/toppage.js
+++ js/toppage.js
@@ -14,6 +14,16 @@ if ($('.top-hello').length) {
 }
 
 
+document.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY === 0) {
+        header.classList.add('transparent');
+    } else {
+        header.classList.remove('transparent');
+    }
+});
+
+
 // 時間ごとのあいさつ
 var d = new Date();
 var hours = d.getHours();
